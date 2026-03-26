a0023650a3a1abd2c6ff6c5b8c67b6b6ad2ca194
elizavetaatis <150339957+elizavetaatis@users.noreply.github.com>
2026-03-26 12:04:05 +0300

    add habits localStorage and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import { useEffect, useState } from "react"
 
-const habits = ["Спорт", "Чтение", "Вода", "Сон до 23:00"]
 
 export default function App() {
-  const [done, setDone] = useState<string[]>(() => {
+  /* Список привычек */
+  const [habits, setHabits] = useState<string[]>(() => {
     const saved = localStorage.getItem('habits')
+    return saved ? JSON.parse(saved) : ["Спорт", "Чтение", "Вода", "Сон до 23:00"]
+  })
+  /* Выполненные привычки */
+  const [done, setDone] = useState<string[]>(() => {
+    const saved = localStorage.getItem('doneHabits')
     return saved ? JSON.parse(saved) : []
   })
-
+  /* Новая привычка */
+  const [newHabit, setNewHabit] = useState('')
+  /* Заполнение списка привычек в localStorage */
   useEffect(() => {
-    localStorage.setItem('habits', JSON.stringify(done))
+    localStorage.setItem('habits', JSON.stringify(habits))
+  }, [habits])
+  /* Сохранение в localStorage */
+  useEffect(() => {
+    localStorage.setItem('doneHabits', JSON.stringify(done))
   }, [done])
+  /* Выполенна/Не выполенна привычка */
   const toggle = (habit: string) => {
     if (done.includes(habit)) {
       setDone(done.filter(h => h !== habit))
@@ -18,18 +30,41 @@ export default function App() {
       setDone([...done, habit])
     }
   }
+  /* Добавление новой привычки */
+  const addHabit = () => {
+    if (newHabit.trim() === '') return
+    setHabits([...habits, newHabit])
+    setNewHabit('')
+  }
 
   return (
     <div className='min-h-screen bg-gray-950 flex items-center justify-center'>
       <div className='bg-gray-900 rounded-2xl p-8 w-full max-w-md shadow-xl'>
         <h1 className="text-white text-2xl font-bold mb-6">Трекер привычек</h1>
+        <div className='flex flex-col items-center gap-3 mb-6'>
+          <span className="text-gray-400 text-sm">Добавить новую привычку:</span>
+          <input
+            type='text'
+            value={newHabit}
+            className="w-full bg-gray-800 text-white rounded-lg px-4 py-2 mt-2"
+            placeholder="Название привычки"
+            onChange={(e) => setNewHabit(e.target.value)}
+            onKeyDown={(e) => e.key === 'Enter' && addHabit()}
+          />
+          <button
+            className="bg-green-500 text-white rounded-lg px-4 py-2 mt-2"
+            onClick={() => addHabit()}
+          >
+            Добавить
+          </button>
+        </div>
         <div className="flex flex-col gap-3">
           {habits.map(habit => (
             <label
               key={habit}
               className={`flex items-center gap-3 p-4 rounded-xl cursor-pointer transition-all ${done.includes(habit)
-                  ? "bg-green-500/20 border border-green-500/40"
-                  : "bg-gray-800 border border-gray-700 hover:border-gray-500"
+                ? "bg-green-500/20 border border-green-500/40"
+                : "bg-gray-800 border border-gray-700 hover:border-gray-500"
                 }`}
             >
               <input
